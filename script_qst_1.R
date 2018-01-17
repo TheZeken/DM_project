73dad302e16f9036d1d3ed0f1a36d3f0c631677e
Jérémie BLANCHARD <jeremieblanchard@free.fr>
2018-01-17 10:58:00 +0100

    First version of script for qst1

---
+++ script_qst_1.R
@@ -0,0 +1,39 @@
+# Load CSV files using the function read.csv
+#AUTO
+auto_bdg_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_AUTO_BDG_2015_2016_2017.csv",sep = ";")
+auto_class_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_AUTO_CLASSIQUE_2015_2016_2017.csv",sep = ";")
+auto_class_tma_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_AUTO_CLASSIQUE_TMA_2016_2017.csv",sep = ";")
+auto_tma_bdg_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_AUTO_TMA_BDG_2016_2017.csv",sep = ";")
+auto_presto_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_AUTOPRESTO_2015_2016_2017.csv",sep = ";")
+
+#DAB
+dab_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_DAB_2015_2016_2107.csv",sep = ";")
+
+#Dentaire
+dent_non_part_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_DENTAIRE_NON_PARTENAIRE_2015_2016_2017.csv",sep = ";")
+dent_part_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_DENTAIRE_PARTENAIRE_2015_2016_2017.csv",sep = ";")
+
+#Modification
+mod_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_MODIFICATION_2015_2016_2017.csv",sep = ";")
+
+#Optique
+opt_non_part_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_OPTIQUE_NON_PARTENAIRE_2015_2016_2017.csv",sep = ";")
+opt_part_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_OPTIQUE_PARTENAIRE_2015_2016_2017.csv",sep = ";")
+
+#Reclamation
+reclam_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_RECLAMATION_2015_2016_2107.csv",sep = ";")
+
+#resilation
+resil_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_RESILIATION_2015_2016_2017.csv",sep = ";")
+
+#Souscription
+sous_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_SOUSCRIPTION_2015_2016_2017.csv",sep = ";")
+
+#suivi proacif
+suiv_15_16_17 <- read.csv("C:/Users/jerem/Desktop/M2/DM_project/big_data_project_confidential/satisfaction/SATISFACTION_SUIVI_PROACTIF_2016_2017.csv",sep = ";")
+
+
+
+
+
+
